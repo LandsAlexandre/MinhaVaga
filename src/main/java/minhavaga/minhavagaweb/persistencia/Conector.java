b66591902a81e0a2995354607a79220641f47d9d
LandsAlexandre <landerson.lma@hotmail.com>
2018-06-01 23:27:34 -0300

    Limpando o código.

--- src/main/java/minhavaga/minhavagaweb/persistencia/Conector.java
+++ src/main/java/minhavaga/minhavagaweb/persistencia/Conector.java
@@ -12,31 +12,7 @@ import java.sql.SQLException;
  */
 public class Conector {
 
-    /* private static final String DRIVER = "org.postgresql.Driver";
-     private static final String URL = "jdbc:postgresql://localhost:5432/MinhaVaga2.0";
-     private static final String USER = "postgres";
-     private static final String PASS = "123";
-    
-     public static Connection getConnection() {
-     try {
-     Class.forName(DRIVER);
-     return DriverManager.getConnection(URL, USER, PASS);
-     } catch (ClassNotFoundException | SQLException ex) {
-     throw new RuntimeException("Erro na conexão: ", ex);
-     }
-     }*/
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        /*
-        String host = "ec2-23-21-129-50.compute-1.amazonaws.com";
-        String database = "d8rgiq86222ua";
-        String user = "kaiveravlpxxhm";
-        String passw = "397bc170e1479cc2aac3018bc3390dce38a1ba4ede0eda67aed6fdd518171309";
-        String port = "5432";
-        Properties props = new Properties();
-        props.setProperty("password", passw);
-        props.setProperty("user", user);
-        props.setProperty("ssl", "true");
-        */
         String dbUrl = System.getenv("DATABASE_URL");
         try {
             Class.forName("org.postgresql.Driver");
