4abf756dd42f9b46ea6b15e7717b50cdd19c3060
Helen <helenfranca93@gmail.com>
2018-06-14 18:35:23 -0300

    Refatorando segundo o SonarQube

--- src/main/java/minhavagaweb/controller/AplCliente.java
+++ src/main/java/minhavagaweb/controller/AplCliente.java
@@ -5,26 +5,12 @@
  */
 package minhavagaweb.controller;
 
-<<<<<<< HEAD
 import minhavagaweb.model.cdp.Cartao;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import minhavagaweb.model.cdp.*;
 import minhavagaweb.model.cgd.*;
-=======
-import minhavagaweb.model.cdp.Cliente;
-import minhavagaweb.model.cdp.CPF;
-import minhavagaweb.model.cdp.Email;
-import minhavagaweb.model.cdp.Pessoa;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import minhavagaweb.model.Cartao;
-import minhavagaweb.model.cdp.ValidaCartao;
-import minhavagaweb.model.cgd.CartaoDAOImpl;
-import minhavagaweb.model.cgd.PessoaDAOImpl;
->>>>>>> 109b7e285f4c924299584f87d0d7e172b484aae8
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,19 +26,6 @@ public class AplCliente {
     
     private static final String SOLICITACAO = "solicitarReserva";
 
-<<<<<<< HEAD
-=======
-    @RequestMapping(value = "home", method = RequestMethod.POST)
-    public ModelAndView home() {
-        return new ModelAndView("home");
-    }
-
-    @RequestMapping("index")
-    public ModelAndView index() {
-        return new ModelAndView("index");
-    }
-
->>>>>>> 109b7e285f4c924299584f87d0d7e172b484aae8
     @RequestMapping(value = "cliente", method = RequestMethod.GET)
     public ModelAndView cliente() {
 
@@ -78,11 +51,7 @@ public class AplCliente {
     @RequestMapping("cliente")
     public String cadastrarCliente(
             Cliente p, @RequestParam("datanascimento") String datanascimento,
-<<<<<<< HEAD
             @RequestParam(value = "cadastrarCartao", required = false) String cadastrar) throws ParseException {
-=======
-            @RequestParam(value = "cadastrarCartao", required = false) String cadastrar) throws SQLException, ClassNotFoundException, ParseException {
->>>>>>> 109b7e285f4c924299584f87d0d7e172b484aae8
 
         PessoaDAOImpl dao = new PessoaDAOImpl();
 
@@ -96,11 +65,7 @@ public class AplCliente {
                 if (cadastrar != null) {
                     return "cartao";
                 } else {
-<<<<<<< HEAD
                     return SOLICITACAO;
-=======
-                    return "solicitarReserva";
->>>>>>> 109b7e285f4c924299584f87d0d7e172b484aae8
                 }
             } catch (ClassNotFoundException | SQLException e) {
                 return "emailRegistrado";
@@ -109,27 +74,10 @@ public class AplCliente {
         return "cliente";
     }
 
-<<<<<<< HEAD
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String fazerLogin(Cliente p) throws SQLException, ClassNotFoundException {
         if (verificarLogin(p.getEmail(), p.getSenha())) {
             return SOLICITACAO;
-=======
-    public String alterarCliente() {
-        return null;
-    }
-
-    @RequestMapping(value = "login", method = RequestMethod.GET)
-    public ModelAndView login() {
-
-        return new ModelAndView("login", "Pessoa", new Pessoa());
-    }
-
-    @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String fazerLogin(Cliente p) throws SQLException, ClassNotFoundException {
-        if (verificarLogin(p.getEmail(), p.getSenha())) {
-            return "solicitarReserva";
->>>>>>> 109b7e285f4c924299584f87d0d7e172b484aae8
         } else {
             return "loginIncorreto";
         }
@@ -140,7 +88,6 @@ public class AplCliente {
         return dao.selectLogin(email, senha);
     }
 
-<<<<<<< HEAD
     /**
      *
      * @param c
@@ -149,23 +96,14 @@ public class AplCliente {
      */
     @RequestMapping("cartao")
     public String cadastrarCartao(Cartao c) throws Exception {
-=======
-    @RequestMapping("cartao")
-    public String cadastrarCartao(Cartao c) throws SQLException, ClassNotFoundException, ParseException, Exception {
->>>>>>> 109b7e285f4c924299584f87d0d7e172b484aae8
 
         CartaoDAOImpl dao = new CartaoDAOImpl();
         if (ValidaCartao.validCC(c.getNumeroCartao())) {
             dao.insert(c);
-<<<<<<< HEAD
             return SOLICITACAO;
-=======
-            return "solicitarReserva";
->>>>>>> 109b7e285f4c924299584f87d0d7e172b484aae8
         } else {
             return "cartao-invalido";
         }
 
     }
-
 }
