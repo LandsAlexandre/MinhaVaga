4a270bd3c17306f7b33c73033af98e987fed247a
LandsAlexandre <landerson.lma@hotmail.com>
2018-06-18 12:59:47 -0300

    Atualizando teste

--- src/test/java/minhavagaweb/teste/CadastrarUsuario.java
+++ src/test/java/minhavagaweb/teste/CadastrarUsuario.java
@@ -8,8 +8,8 @@ package minhavagaweb.teste;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import minhavagaweb.controller.AplCliente;
 import minhavagaweb.model.cdp.*;
+import minhavagaweb.model.cgt.AplCliente;
 import static org.junit.Assert.assertEquals;
 
 /**
